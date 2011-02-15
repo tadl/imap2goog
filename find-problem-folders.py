855ea7d88788fe99d5bf8d1df4298980fd18a967
Jeff Godin <jgodin@tadl.org>
2011-02-15 17:14:08 -0500

    TADL IMAP -> Google Apps migration tools. Support library, sample config files, "preflight" script for finding problem folders before migrating.

---
+++ find-problem-folders.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+from imaplib import IMAP4, ParseFlags
+import re
+from tadl.imap import *
+
+from optparse import OptionParser
+op = OptionParser()
+op.add_option("-u", "--user", dest="user")
+op.add_option("-v", "--verbose", action="store_true", dest="verbose")
+(options, args) = op.parse_args()
+if (options.user == None):
+    op.error('-u/--user argument is required')
+
+verbose = False
+if (options.verbose):
+    verbose = True
+
+import ConfigParser
+config = ConfigParser.ConfigParser()
+config.read('migration.cfg')
+imap_server = config.get('IMAP', 'server')
+
+imap_user = options.user
+imap_password = getCredentials(imap_user, 'credentials.csv')
+
+imap = IMAP4(imap_server)
+imap.login(imap_user, imap_password)
+imap.debug = 2
+
+folders = getAllFolders(imap)
+
+folderlist = []
+
+for folder in folders:
+    flags, root, name = folder
+    count = getMessageCount(imap, name)
+    if (verbose):
+        print 'Folder "%s" has %s messages.' % (name, count)
+    # stuff all source folder names into list
+    folderlist.append(name)
+    if (count == 4962):
+        print "Source folder has max messages (4962): %s" % name
+
+folderlist_transformed = []
+for fn in folderlist:
+    new_fn = re.sub(r'^Cabinet/','',fn)
+    folderlist_transformed.append(new_fn)
+    if (verbose):
+        print "Folder %s => %s" % (fn, new_fn)
+
+fn_dict = {}
+# count each folder name after transformation
+for folder in folderlist_transformed:
+    try:
+        fn_dict[folder] += 1
+    except:
+        fn_dict[folder] = 1
+
+# warn about any collisions (where the transformed name matched an existing)
+for key in fn_dict.keys():
+    if (fn_dict[key] > 1):
+        print "Potential collision with folder name %s" % key
+    if (len(key) > 40):
+        print "Transformed folder name is >40 chars: %s" % key
+
+
---
+++ tadl/imap.py
@@ -0,0 +1,54 @@
+import re
+from imaplib import ParseFlags
+
+def parseFolder(folder_data):
+    m = re.search('^(\(.*\)) "(.*)" "(.*)"', folder_data)
+    return m.group(1), m.group(2), m.group(3)
+
+def getAllFolders(imap_connection):
+    i = imap_connection
+    folder_data = []
+    stat, list = i.list()
+    if (stat != "OK"):
+        raise Exception('list failed')
+    for folder in list:
+        flags, root, name = parseFolder(folder)
+        folder_tuple = (flags, root, name)
+        folder_data.append(folder_tuple)
+    return folder_data
+
+def getMessageCount(imap_connection, folder_name):
+    i = imap_connection
+    folder = folder_name
+    stat, data = i.select(folder, 1)
+    if (stat != "OK"):
+        raise Exception('select failed')
+    count = int(data[0])
+    return count
+
+def getFlags(imap_connection):
+    i = imap_connection
+    flag_data = []
+    status, data = i.uid("fetch", "1:*", "(FLAGS)")
+    if (data == [None]):
+        return []
+    for record in data:
+        #print "record: %s" % record
+        m = re.match("^(\d+) \(FLAGS \(.*\) UID (.*)\)$", record)
+        num = m.group(1)
+        uid = m.group(2)
+        flags = ParseFlags(record)
+        flag_tuple = (uid, flags)
+        flag_data.append(flag_tuple)
+        #print "message %s has uid %s, flags %s" % (num, uid, flags)
+    return flag_data
+
+import csv
+
+def getCredentials(username, filename):
+    f = file(filename, 'r')
+    r = csv.DictReader(f)
+    for dict in r:
+        if (dict['user'] == username):
+            return dict['password']
+
