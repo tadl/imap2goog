a8350b22e6998888f550e7ce7d9a17c5c0651dd0
Jeff Godin <jgodin@tadl.org>
2011-03-28 11:44:19 -0400

    Extract to mbox, vcalendar files
    Extract all items in Calendar folder to an mbox file, also output
    all text/calendar parts to a .vcalendar file as a series of VEVENT
    elements contained within a single VCALENDAR element.
    
    Initial tests have shown that it might be useful to exclude long-
    past events, and/or to split the events across multiple files
    before importing to Google Calendar.
    
    Currently, no warning is generated when a message in the
    Calendar folder does not contain a text/calendar part.

--- calendar-extract.py
+++ calendar-extract.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 from imaplib import IMAP4
+import email
+import re
+from cStringIO import StringIO
+from email.generator import Generator
+
 from tadl.imap import *
 
 import ConfigParser
@@ -30,3 +35,45 @@ imap.debug = 2
 cal_count = getMessageCount(imap, "Calendar")
 
 print cal_count, "items in Calendar folder."
+
+messageflags = getFlags(imap)
+
+def msg2string(msg, unixfrom=False):
+    fp = StringIO()
+    g = Generator(fp, mangle_from_=False)
+    g.flatten(msg, unixfrom=unixfrom)
+    text = fp.getvalue()
+    return text
+
+def SaveMessage(message):
+    mbox_filename = imap_user + '.mbox'
+    mbox_target = open(mbox_filename, 'ab')
+    mbox_target.write(msg2string(message, unixfrom=True))
+    mbox_target.close()
+
+vcalendar_filename = imap_user + '.vcalendar'
+vcalendar_target = open(vcalendar_filename, 'wb')
+
+vcalendar_target.write('BEGIN:VCALENDAR\n')
+vcalendar_target.write('VERSION:2.0\n')
+
+for (uid, flags) in messageflags:
+    stat, data = imap.uid("fetch", uid, '(RFC822)')
+    msgstr = data[0][1]
+    msg = email.message_from_string(msgstr)
+    SaveMessage(msg)
+    part_iter = email.iterators.typed_subpart_iterator(msg, 'text', 'calendar')
+    for part in part_iter:
+        vcal = part.get_payload(decode=True)
+        vcal = re.sub(r'\r\n', '\n', vcal)
+        for line in vcal.splitlines():
+            if re.search(r'^BEGIN:VCALENDAR', line):
+                continue
+            if re.search(r'^VERSION:', line):
+                continue
+            if re.search(r'^PRODID:', line):
+                continue
+            if re.search(r'^END:VCALENDAR', line):
+                continue
+            vcalendar_target.write(line + '\n')
+vcalendar_target.write('END:VCALENDAR\n')
